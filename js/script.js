f2bca262eb6e152f65dac5290f468b2ed7b70474
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-09-22 03:08:32 +0800

    Create ContainerS containing several divs 1- Have a prompt function that requests a number X; 2- Create a X number of containers; 3- Fill the containers of X number of divs;

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-const container = document.getElementById('container');
+// const container = document.getElementById('container');
+const containers = [];
 const divList = [];
-
-for (i=0; i < 256 ; i++){ // create Nodelist of divs
-    divList[i] = document.createElement('div');
-    divList[i].setAttribute('class', 'square');
-    container.append(divList[i])
+function numOfSquares(){
+    let squareNum = prompt('How big?', '100 MAX')
+for (i=0; i < squareNum ; i++){ 
+    const body = document.querySelector('body');
+    containers[i] = document.createElement('div');
+    containers[i].setAttribute('class', 'container')
+    for (y=0; y < squareNum ; y++){
+        divList[y] = document.createElement('div');
+        divList[y].setAttribute('class', 'square');
+        containers[i].append(divList[y])
+    }
+    body.append(containers[i])
 };
-
-for (i=0; i < 256 ; i++){ // Hover function to add and remove background
-    const collection = document.getElementsByClassName("square");
-    collection[i].addEventListener("mouseover", hoverOn);
-    collection[i].addEventListener("mouseout", hoverOut);
-}
+};
+// for (i=0; i < 16 ; i++){ // Hover function to add and remove background
+//     const collection = document.getElementsByClassName("square");
+//     collection[i].addEventListener("mouseover", hoverOn);
+//     collection[i].addEventListener("mouseout", hoverOut);
+// }
 
 function hoverOn(){
     this.style.backgroundColor = "black";
