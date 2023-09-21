4f9e743f4c91db29cbe291cf9cba9d5e71d82f3d
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-09-22 02:28:02 +0800

    Add 16X16 square divs with hover effects 1- 256 divs inside a container 2- mouseon effect changing background to black 3- mouseout effect changing background to white

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 const container = document.getElementById('container');
-const div1 = document.createElement('div');
-const divList = []
-for (i=0; i<16 ; i++){
-    divList.push(`div${i}`)
-}
-console.log(divList);
+const divList = [];
 
-for (i=0; i< divList.length ; i++){
+for (i=0; i < 256 ; i++){ // create Nodelist of divs
     divList[i] = document.createElement('div');
+    divList[i].setAttribute('class', 'square');
     container.append(divList[i])
+};
+
+for (i=0; i < 256 ; i++){ // Hover function to add and remove background
+    const collection = document.getElementsByClassName("square");
+    collection[i].addEventListener("mouseover", hoverOn);
+    collection[i].addEventListener("mouseout", hoverOut);
 }
 
+function hoverOn(){
+    this.style.backgroundColor = "black";
+};
+function hoverOut(){
+    this.style.backgroundColor = "white";
+};
 
+// Make a button that sets the number of squares needed.
 
+const btn = document.querySelector('button');
+btn.addEventListener('click', numOfSquares);
 
 
