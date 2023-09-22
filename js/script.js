5683d0d4a227eae6a2329c6b1a6683f9cda385e6
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-09-22 11:35:19 +0800

    Create all html elements (btns + divs) using js.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 // const container = document.getElementById('container');
 const containers = [];
 const divList = [];
+const body = document.querySelector('body')
+const btnContainer = document.createElement('div');
+const btnStart = document.createElement('button');
+const btnClear = document.createElement('button');
+btnStart.innerText = 'Dimensions';
+btnClear.innerText = 'Clear';
+btnContainer.setAttribute('class', 'btns-css')
+btnStart.setAttribute('class','start');
+btnClear.setAttribute('class','clear')
+body.append(btnContainer);
+btnContainer.append(btnStart);
+btnContainer.append(btnClear);
+
+
+
+
 function numOfSquares(){
     let squareNum = prompt('How big?', '100 MAX')
 for (i=0; i < squareNum ; i++){ 
@@ -10,27 +26,30 @@ for (i=0; i < squareNum ; i++){
     for (y=0; y < squareNum ; y++){
         divList[y] = document.createElement('div');
         divList[y].setAttribute('class', 'square');
+        divList[y].addEventListener('mouseover', hoverOn);
         containers[i].append(divList[y])
-    }
-    box.append(containers[i])
+    };
+    box.append(containers[i]);
+    };
 };
-};
-// for (i=0; i < 16 ; i++){ // Hover function to add and remove background
-//     const collection = document.getElementsByClassName("square");
-//     collection[i].addEventListener("mouseover", hoverOn);
-//     collection[i].addEventListener("mouseout", hoverOut);
-// }
+
+
+
 
 function hoverOn(){
     this.style.backgroundColor = "black";
 };
-function hoverOut(){
-    this.style.backgroundColor = "white";
+function clearBox(){
+    const restart = document.querySelectorAll('.container');
+    for (i=0; i<restart.length ; i++){
+        restart[i].remove()
+    }
 };
 
 // Make a button that sets the number of squares needed.
-
-const btn = document.querySelector('button');
+const btn = document.querySelector('.start');
 btn.addEventListener('click', numOfSquares);
 
-
+// Make a button that resets the entire box.
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clearBox);
