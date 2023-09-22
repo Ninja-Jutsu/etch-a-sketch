1166ef54a9549fbf0ee01cb4ee5a7ec22eb1a164
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-09-23 04:08:13 +0800

    Add Color Picker + Rainbow Mode

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 // const container = document.getElementById('container');
-const containers = [];
-const divList = [];
+const containers = []
+const divList = []
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+
+const box = document.querySelector('.box')
+const setBtn = document.querySelector('.start')
+const clearBtn = document.querySelector('.clear')
+const colorPicker = document.getElementById('colorPicker')
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+setBtn.onclick = () => numOfSquares()
+clearBtn.onclick = () =>clearBox()
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+
 
 function numOfSquares(){
     let squareNum = prompt('Insert a valid number (Max 100)', 100);
-
     if (squareNum<=100){
+        box.innerHTML = '';
         for (i=0; i < squareNum ; i++){ 
-            const box = document.querySelector('.box');
             containers[i] = document.createElement('div');
             containers[i].setAttribute('class', 'container')
             for (y=0; y < squareNum ; y++){
@@ -24,8 +42,19 @@ function numOfSquares(){
 };
 
 function hoverOn(){
-    this.style.backgroundColor = "black";
+    console.log(currentMode)
+    if (currentMode !== 'eraser'){
+    this.style.backgroundColor = currentColor;}
+    else if (currentMode === 'eraser'){
+    this.style.backgroundColor = '#efefce';
+    }
+    if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        this.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`}
 };
+
 function clearBox(){
     const restart = document.querySelectorAll('.container');
     for (i=0; i<restart.length ; i++){
@@ -33,10 +62,33 @@ function clearBox(){
     }
 };
 
-// Make a button that sets the number of squares needed.
-const btn = document.querySelector('.start');
-btn.addEventListener('click', numOfSquares);
+function setCurrentColor(newColor) {
+    currentColor = newColor
+  }
 
-// Make a button that resets the entire box.
-const clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', clearBox);
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+  }
+
+function activateButton(newMode) {
+if (currentMode === 'rainbow') {
+    rainbowBtn.classList.remove('active')
+} else if (currentMode === 'color') {
+    colorBtn.classList.remove('active')
+} else if (currentMode === 'eraser') {
+    eraserBtn.classList.remove('active')
+}
+
+if (newMode === 'rainbow') {
+    rainbowBtn.classList.add('active')
+} else if (newMode === 'color') {
+    colorBtn.classList.add('active')
+} else if (newMode === 'eraser') {
+    eraserBtn.classList.add('active')
+}
+}
+
+window.onload = () => {
+    activateButton(DEFAULT_MODE)
+  }
